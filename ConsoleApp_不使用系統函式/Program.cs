b687e779700ce8847580bba3915d01bec5b1e362
joe lee <as955133@gmail.com>
2018-05-24 23:13:01 +0800

    第一版，失敗

--- ConsoleApp_不使用系統函式/Program.cs
+++ ConsoleApp_不使用系統函式/Program.cs
@@ -10,6 +10,19 @@ namespace ConsoleApp_不使用系統函式
     {
         static void Main(string[] args)
         {
+            int[] tAry = new int[] { 7, 1, 8, 10, 3, 5, 2, 11, 6, 4, 9 };
+
+            for(int i = 0; i <= tAry.Length; i++)
+            {
+                for(int k = i; k <= tAry.Length; k++)
+                {
+                    if(k > i)
+                    {
+                        Console.WriteLine($"{k}");
+                    }
+                }
+                Console.ReadLine();
+            }
         }
     }
 }
